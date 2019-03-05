e743a99c69470f3876f5efa53cf5620cd3c8daa3
Arthur Câmara <A.BarbosaCamara@tudelft.nl>
2019-03-05 15:50:07 +0100

    added check of number of process

--- IR-Baseline/main.py
+++ IR-Baseline/main.py
@@ -15,7 +15,7 @@ from contextlib import closing
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path',)
 parser.add_argument("-t", "--train", action="store_true")
-parser.add_argument("-m", "--multiprocess", action="store_true")
+parser.add_argument("-m", "--multiprocess", argument_default="2")
 
 def process_line(line):
     line_data = json.loads(line)
@@ -30,37 +30,19 @@ def process_line(line):
 
 if __name__=="__main__":
 
-    #First, calculate IDFs for trainning datasets
     args = parser.parse_args()
     TF_IDF_transformer = TF_IDF_Documents(args.path, force_train=args.train)
 
-    # For each document in the test dataset, get the TF-IDF representation of the query and the documents
-
-    matches = []    
-    #TODO paralelize this
-
-    if args.multiprocess:
+    matches = [] 
+    if args.multiprocess>1:
         lines_to_process = [x for x in open(TF_IDF_transformer.test_path).readlines()]
-        with closing(multiprocessing.Pool()) as pool:
+        with closing(multiprocessing.Pool(args.multiprocess)) as pool:
             matches = pool.map(process_line, lines_to_process)
             pool.terminate()
-    for line in tqdm(open(TF_IDF_transformer.test_path).readlines()):
-        matches.append(process_line(line))
-        # line_data = json.loads(line)
-        # query = query_cleaner(line_data['phrase'])
-        # page = line_data["webpage"]
-        # docs_vectors, page_docs = TF_IDF_transformer.load_docs_for_page(page)
-        # query_vector = TF_IDF_transformer.get_query_vector(query)
-        # similarity_matrix = cosine_similarity(query_vector, docs_vectors)
-        # best_match = similarity_matrix.argmax()
-        # target = line_data['xid']
-        # matches.append(target==best_match)
-        if len(matches) % 100 ==0:
-            print("current accuracy {}".format((sum(matches)*1.0)/len(matches)))
+    else:
+        for line in tqdm(open(TF_IDF_transformer.test_path).readlines()):
+            matches.append(process_line(line))
+            if len(matches) % 100 ==0:
+                print("current accuracy {}".format((sum(matches)*1.0)/len(matches)))
     print("current accuracy {}".format((sum(matches)*1.0)/len(matches)))
     
-    # #for each query in the TEST dataset, go to the target page, compute TF-IDF for each document, store the document on dictionary
-    # for counter, line in enumerate(open(train_queries_path)):
-    #     if counter > 100:
-    #         break
-    # # documenst_path = os.path.join(args.path, "phrase-node-dataset","infos", "v6")
