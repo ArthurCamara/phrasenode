eeec4cf403c42d730a33cf439461cd4f2bf68538
Arthur Câmara <A.BarbosaCamara@tudelft.nl>
2019-03-05 15:16:32 +0100

    parallel testing

--- IR-Baseline/main.py
+++ IR-Baseline/main.py
@@ -9,10 +9,24 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from tf_idf_documents import TF_IDF_Documents
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
+import multiprocessing
+from contextlib import closing
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path',)
 parser.add_argument("-t", "--train", action="store_true")
+parser.add_argument("-m", "--multiprocess", action="store_true")
+
+def process_line(line):
+    line_data = json.loads(line)
+    query = query_cleaner(line_data['phrase'])
+    page = line_data["webpage"]
+    docs_vectors, page_docs = TF_IDF_transformer.load_docs_for_page(page)
+    query_vector = TF_IDF_transformer.get_query_vector(query)
+    similarity_matrix = cosine_similarity(query_vector, docs_vectors)
+    best_match = similarity_matrix.argmax()
+    target = line_data['xid']
+    return target==best_match
 
 if __name__=="__main__":
 
@@ -24,16 +38,23 @@ if __name__=="__main__":
 
     matches = []    
     #TODO paralelize this
+
+    if args.multiprocess:
+        lines_to_process = [x for x in open(TF_IDF_transformer.test_path).readlines()]
+        with closing(multiprocessing.Pool()) as pool:
+            matches = pool.map(process_line, lines_to_process)
+            pool.terminate()
     for line in tqdm(open(TF_IDF_transformer.test_path).readlines()):
-        line_data = json.loads(line)
-        query = query_cleaner(line_data['phrase'])
-        page = line_data["webpage"]
-        docs_vectors, page_docs = TF_IDF_transformer.load_docs_for_page(page)
-        query_vector = TF_IDF_transformer.get_query_vector(query)
-        similarity_matrix = cosine_similarity(query_vector, docs_vectors)
-        best_match = similarity_matrix.argmax()
-        target = line_data['xid']
-        matches.append(target==best_match)
+        matches.append(process_line(line))
+        # line_data = json.loads(line)
+        # query = query_cleaner(line_data['phrase'])
+        # page = line_data["webpage"]
+        # docs_vectors, page_docs = TF_IDF_transformer.load_docs_for_page(page)
+        # query_vector = TF_IDF_transformer.get_query_vector(query)
+        # similarity_matrix = cosine_similarity(query_vector, docs_vectors)
+        # best_match = similarity_matrix.argmax()
+        # target = line_data['xid']
+        # matches.append(target==best_match)
         if len(matches) % 100 ==0:
             print("current accuracy {}".format((sum(matches)*1.0)/len(matches)))
     print("current accuracy {}".format((sum(matches)*1.0)/len(matches)))
